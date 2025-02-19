cd959cd2885e4c10e5d26d86ab1d040bed7494d2
harshini <harshinipamarthi@gmail.com>
2025-02-19 20:19:05 +0530

    node

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "google_container_cluster" "primary" {
     name = var.GKE_CLUSTER 
     location = var.GKE_REGION
+    deletion_protection = false
+    remove_default_node_pool = true 
     initial_node_count = var.NODE_COUNT
-
-
     node_config {
         machine_type = var.MACHINE_TYPE 
         disk_size_gb = 30
@@ -11,7 +11,7 @@ resource "google_container_cluster" "primary" {
             "https://www.googleapis.com/auth/cloud-platform"
         ]
     }
-    remove_default_node_pool = true 
+    
 }
 
 
